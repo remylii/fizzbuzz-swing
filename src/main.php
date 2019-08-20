9963409c58660178527bc0826be265143d3f9c1a
remyli <rapi.remyli@gmail.com>
2019-08-20 12:20:06 +0900

    fisrtstep

--- src/main.php
+++ src/main.php
@@ -1,2 +1,30 @@
 <?php
-echo "Hello, world" . PHP_EOL;
+namespace FizzBuzzSwing;
+
+class Main
+{
+    public function fizzbuzz()
+    {
+        for ($i = 1; $i <= 15; $i++) {
+            $result = '';
+
+            if ($i%3 === 0) {
+                $result .= 'Fizz';
+            }
+
+            if ($i%5 === 0) {
+                $result .= 'Buzz';
+            }
+
+            if ($result === '') {
+                $result = $i;
+            }
+
+            echo $result;
+            unset($result);
+        }
+    }
+}
+
+$m = new Main();
+$m->fizzbuzz();
--- tests/MainTest.php
+++ tests/MainTest.php
@@ -7,6 +7,12 @@ class MainTest extends TestCase
 {
     public function testMain()
     {
-        $this->assertTrue(true);
+        ob_start();
+
+        require_once(__DIR__ . '/../src/main.php');
+
+        $output_contents = ob_get_clean();
+
+        $this->assertEquals("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz", $output_contents);
     }
 }
