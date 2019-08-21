688a27ce4e015b4d5efbcf1d8b1ed7a54f4dc840
remyli <rapi.remyli@gmail.com>
2019-08-21 12:26:03 +0900

    関数に分割

--- src/main.php
+++ src/main.php
@@ -6,22 +6,25 @@ class Main
     public function boot()
     {
         for ($i = 1; $i <= 15; $i++) {
-            $result = '';
-
-            if ($i%3 === 0) {
-                $result .= 'Fizz';
-            }
-
-            if ($i%5 === 0) {
-                $result .= 'Buzz';
-            }
-
-            if ($result === '') {
-                $result = $i;
-            }
-
-            echo $result;
-            unset($result);
+            echo $this->fizzbuzz($i);
         }
+    }
+
+    public function fizzbuzz($n)
+    {
+        $res = '';
+        if ($n%3 === 0) {
+            $res .= 'Fizz';
+        }
+
+        if ($n%5 === 0) {
+            $res .= 'Buzz';
+        }
+
+        if ($res === '') {
+            $res = $n;
+        }
+
+        return $res;
     }
 }
--- tests/MainTest.php
+++ tests/MainTest.php
@@ -6,7 +6,7 @@ use FizzBuzzSwing\Main;
 
 class MainTest extends TestCase
 {
-    public function testMain()
+    public function testBoot()
     {
         ob_start();
 
@@ -16,5 +16,15 @@ class MainTest extends TestCase
         $output_contents = ob_get_clean();
 
         $this->assertEquals("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz", $output_contents);
+    }
+
+    public function testFizzbuzz()
+    {
+        $m = new Main();
+        $this->assertEquals(1, $m->fizzbuzz(1));
+        $this->assertEquals('Fizz', $m->fizzbuzz(3));
+        $this->assertEquals('Buzz', $m->fizzbuzz(5));
+        $this->assertEquals('FizzBuzz', $m->fizzbuzz(15));
+        $this->assertEquals('Fizz', $m->fizzbuzz(99999));
     }
 }
